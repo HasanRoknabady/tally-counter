2939856c4ffc410a955722b7474f3da862d5dc33
ali <al.aliabadi@mail.sbu.ac.ir>
2021-02-15 01:12:29 +0330

    bugfix

--- src/main/java/sbu/cs/ExerciseLecture4.java
+++ src/main/java/sbu/cs/ExerciseLecture4.java
@@ -3,50 +3,50 @@ package sbu.cs;
 public class ExerciseLecture4 {
 
     /*
-    *   implement a function that returns factorial of given n
-    *   lecture 4 page 15
+     *   implement a function that returns factorial of given n
+     *   lecture 4 page 15
      */
     public long factorial(int n) {
         return 0L;
     }
 
     /*
-    *   implement a function that return nth number of fibonacci series
-    *   the series -> 1, 1, 2, 3, 5, 8, ...
-    *   lecture 4 page 19
+     *   implement a function that return nth number of fibonacci series
+     *   the series -> 1, 1, 2, 3, 5, 8, ...
+     *   lecture 4 page 19
      */
-    public int fibonacci(int n) {
+    public long fibonacci(int n) {
         return 0;
     }
 
     /*
-    *   implement a function that return reverse of a given word
-    *   lecture 4 page 19
+     *   implement a function that return reverse of a given word
+     *   lecture 4 page 19
      */
     public String reverse(String word) {
         return null;
     }
 
     /*
-    *   implement a function that returns true if the given line is
-    *   palindrome and false if it is not palindrome.
-    *   palindrome is like wow, never odd or even
-    *   lecture 4 page 19
+     *   implement a function that returns true if the given line is
+     *   palindrome and false if it is not palindrome.
+     *   palindrome is like wow, never odd or even
+     *   lecture 4 page 19
      */
     public boolean isPalindrome(String line) {
         return false;
     }
 
     /*
-    *   implement a function which computes the dot plot of 2 given
-    *   string. dot plot of hello and ali is:
-    *       h e l l o
-    *   h   *
-    *   e     *
-    *   l       * *
-    *   l       * *
-    *   o           *
-    *   lecture 4 page 26
+     *   implement a function which computes the dot plot of 2 given
+     *   string. dot plot of hello and ali is:
+     *       h e l l o
+     *   h   *
+     *   e     *
+     *   l       * *
+     *   l       * *
+     *   o           *
+     *   lecture 4 page 26
      */
     public char[][] dotPlot(String str1, String str2) {
         return null;
--- src/main/java/sbu/cs/ExerciseLecture5.java
+++ src/main/java/sbu/cs/ExerciseLecture5.java
@@ -3,30 +3,30 @@ package sbu.cs;
 public class ExerciseLecture5 {
 
     /*
-    *   implement a function to create a random password with
-    *   given length using lower case letters
-    *   lecture 5 page 14
+     *   implement a function to create a random password with
+     *   given length using lower case letters
+     *   lecture 5 page 14
      */
     public String weakPassword(int length) {
         return null;
     }
 
     /*
-    *   implement a function to create a random password with
-    *   given length and at least 1 digit and 1 special character
-    *   lecture 5 page 14
+     *   implement a function to create a random password with
+     *   given length and at least 1 digit and 1 special character
+     *   lecture 5 page 14
      */
     public String StrongPassword(int length) {
         return null;
     }
 
     /*
-    *   implement a function that checks if a integer is a fibobin number
-    *   integer n is fibobin is there exist an i where:
-    *       n = fib(i) + bin(fib(i))
-    *   where fib(i) is the ith fibonacci number and bin(i) is the number
-    *   of ones in binary format
-    *   lecture 5 page 17
+     *   implement a function that checks if a integer is a fibobin number
+     *   integer n is fibobin is there exist an i where:
+     *       n = fib(i) + bin(fib(i))
+     *   where fib(i) is the ith fibonacci number and bin(i) is the number
+     *   of ones in binary format
+     *   lecture 5 page 17
      */
     public boolean isFiboBin(int n) {
         return false;
